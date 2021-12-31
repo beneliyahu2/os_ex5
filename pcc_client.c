b8c2b7b3561b71a6b2c30cf6e1e707e92b3f64e8
beneliyahu2 <beneliyahu2@mail.tau.ac.il>
2021-12-31 17:13:51 +0200

    beginning

--- pcc_client.c
+++ pcc_client.c
@@ -1,4 +1,30 @@
 //
 // Created by Abigail Ben Eliyahu on Dec 2021.
 //
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <errno.h>
+
+int main(int argc, char *argv[]){
+
+    if (argc != 3){
+        fprintf(stderr, "wrong number of arguments.\n");
+        exit(1);
+    }
+    char *ip_add = argv[1];
+    ushort port = strtol(argv[2], NULL, NULL);
+    char *file_path = argv[3];
+
+
+
+}
+
 
--- pcc_server.c
+++ pcc_server.c
@@ -1,4 +1,63 @@
 //
 // Created by Abigail Ben Eliyahu on Dec 2021.
 //
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+#include <assert.h>
+
+
+int main(int argc, char *argv[]){
+
+    // declaring variables for returned values:
+    int totalsent = -1;
+    int nsent     = -1;
+    int len       = -1;
+    int n         =  0;
+    int listenfd  = -1;
+    int connfd    = -1;
+
+    // initalizing data structure to collect statistics from all clients:
+    int chars_stat[256]; // todo malloc?
+
+    // parse command line arguments:
+    if (argc != 1){
+        fprintf(stderr, "wrong number of arguments.\n");
+        exit(1);
+    }
+    ushort port = strtol(argv[1], NULL, NULL);
+
+    // create a socket to listen to:
+    listenfd = socket( AF_INET, SOCK_STREAM, 0 );
+
+    //create a struct for the binding:
+    //todo
+
+    // bind the socket to the ip address:
+    if( 0 != bind( listenfd, (struct sockaddr*) &serv_addr, addrsize ) )
+    {
+        printf("\n Error : Bind Failed. %s \n", strerror(errno));
+        return 1;
+    }
+
+    // start listening:
+    if( 0 != listen( listenfd, 10 ) ) //maximum 10 pending clients in the queue
+    {
+        printf("\n Error : Listen Failed. %s \n", strerror(errno));
+        return 1;
+    }
+
+    // serving clients from the queue:
+    while( 1 ) {
+        // Accept a connection.
+        connfd = accept( listenfd, NULL, NULL); //todo change to "curr_client_sd"
+
+}
 
