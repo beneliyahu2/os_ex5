a820505a013aec9f8d97b358822744497d1a527b
beneliyahu2 <beneliyahu2@mail.tau.ac.il>
2022-01-01 15:21:55 +0200

    baseline. client sent a msg and server prints it. hard coded msg and its length.

--- pcc_client.c
+++ pcc_client.c
@@ -14,16 +14,60 @@
 #include <errno.h>
 
 int main(int argc, char *argv[]){
+    // declaring variables for returned values:
+    int sockfd = -1;
 
-    if (argc != 3){
-        fprintf(stderr, "wrong number of arguments.\n");
-        exit(1);
+    // parse command line arguments: todo uncomment
+//    if (argc != 3){
+//        fprintf(stderr, "wrong number of arguments.\n");
+//        exit(1);
+//    }
+//    char *ip_add = argv[1];
+//    u_int16_t port = strtol(argv[2], NULL, 10);
+//    char *file_path = argv[3];
+
+    // create a socket:
+    if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Error : Could not create socket \n");
+        return 1;
     }
-    char *ip_add = argv[1];
-    ushort port = strtol(argv[2], NULL, NULL);
-    char *file_path = argv[3];
 
+    //create a struct for connecting:
+    struct sockaddr_in serv_addr; // where we Want to get to
+    //socklen_t addrsize = sizeof(struct sockaddr_in ); todo check why need that
 
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(10000); // Note: htons for endiannes
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // the local host
+
+    // connect socket to the server:
+    if( connect(sockfd, (struct sockaddr*) &serv_addr,sizeof(serv_addr)) < 0){
+        printf("\n Error : Connect Failed. %s \n", strerror(errno));
+        return 1;
+    }
+
+    //msg: - temp todo del
+    char *msg = "the massage from the client.";
+    u_int32_t msg_len = htons(strlen(msg));
+    ssize_t n_written = 0;
+
+    //sending the length of the msg: todo uncomment
+//    while( n_written <  sizeof(msg_len)){
+//        n_written = write(sockfd, msg, 28);
+//    }
+//    n_written = 0;
+
+    // sending the message:
+    while( n_written <  msg_len){
+       n_written = write(sockfd, msg, 28);
+    }
+
+    // closing the socket:
+    close(sockfd);
+
+    return 0; //todo - need this?
 
 }
 
--- pcc_server.c
+++ pcc_server.c
@@ -17,28 +17,34 @@
 int main(int argc, char *argv[]){
 
     // declaring variables for returned values:
-    int totalsent = -1;
-    int nsent     = -1;
-    int len       = -1;
-    int n         =  0;
     int listenfd  = -1;
     int connfd    = -1;
 
     // initalizing data structure to collect statistics from all clients:
-    int chars_stat[256]; // todo malloc?
+//    int chars_stat[256]; todo uncomment
 
-    // parse command line arguments:
-    if (argc != 1){
-        fprintf(stderr, "wrong number of arguments.\n");
-        exit(1);
-    }
-    ushort port = strtol(argv[1], NULL, NULL);
+    // parse command line arguments: todo uncomment
+//    if (argc != 1){
+//        fprintf(stderr, "wrong number of arguments.\n");
+//        exit(1);
+//    }
+//    u_int16_t port_num = strtol(argv[1], NULL, 10);
 
     // create a socket to listen to:
-    listenfd = socket( AF_INET, SOCK_STREAM, 0 );
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if( listenfd < 0){
+        printf("\n Error : Could not create socket \n");
+        return 1;
+    }
 
     //create a struct for the binding:
-    //todo
+    struct sockaddr_in serv_addr;
+    socklen_t addrsize = sizeof(struct sockaddr_in );
+
+    memset( &serv_addr, 0, addrsize ); //starting with clean (zero values)
+    serv_addr.sin_family = AF_INET; //
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY = any local machine address
+    serv_addr.sin_port = htons(10000); //port number 10000
 
     // bind the socket to the ip address:
     if( 0 != bind( listenfd, (struct sockaddr*) &serv_addr, addrsize ) )
@@ -57,7 +63,34 @@ int main(int argc, char *argv[]){
     // serving clients from the queue:
     while( 1 ) {
         // Accept a connection.
-        connfd = accept( listenfd, NULL, NULL); //todo change to "curr_client_sd"
+        connfd = accept( listenfd, NULL, NULL); //todo change name to "curr_client_sd"
+        if( connfd < 0 ){
+            printf("\n Error : Accept Failed. %s \n", strerror(errno));
+            return 1;
+        }
 
+        // buff to accept the msg: todo del
+        u_int32_t msg_len;
+        char buff[1028];
+        ssize_t bytes_received = 0;
+
+//        //read the msg length: todo uncomment
+//        while (bytes_received < sizeof(msg_len)){
+//            bytes_received = read(connfd, &msg_len, sizeof(msg_len));
+//        }
+
+        //read the msg:
+        bytes_received = 0;
+        while (bytes_received < 28){
+            bytes_received = read(connfd, buff, 28);
+        }
+        buff[bytes_received] = '\0';
+
+        //print msg: todo del
+        printf("%s\n", buff);
+
+        // close socket
+        close(connfd);
+    }
 }
 
